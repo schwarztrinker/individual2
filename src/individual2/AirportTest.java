d7235f36f21279ad05df7c351f1bcb441774a7af
schwarztrinker <38423981+schwarztrinker@users.noreply.github.com>
2018-12-20 19:32:03 +0100

    Junit

---
+++ src/individual2/AirportTest.java
@@ -0,0 +1,14 @@
+package individual2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AirportTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/individual2/BaggageSortingUnitTest.java
@@ -0,0 +1,14 @@
+package individual2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BaggageSortingUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
